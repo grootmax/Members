d655a270be4465405d1cc17f47f100c3c1841b23
grootmax <58818000+grootmax@users.noreply.github.com>
2024-02-13 22:57:02 +0530

    Add files via upload

---
+++ db/migrate/20240213102422_create_members.rb
@@ -0,0 +1,10 @@
+class CreateMembers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :members do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
